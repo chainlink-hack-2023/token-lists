6b4050a4202f10b507fc0f93f1b7bddb1076d608
Lecky Lao <leckylao@gmail.com>
2023-11-30 10:51:12 +1100

    feat(Network): Add Fuji tokens

--- src/permitInfo/const.ts
+++ src/permitInfo/const.ts
@@ -8,6 +8,23 @@ export const DEFAULT_RPC_URLS: Record<number, string> = {
   1: 'https://mainnet.infura.io/v3/' + env.INFURA_API_KEY,
   5: 'https://goerli.infura.io/v3/' + env.INFURA_API_KEY,
   100: 'https://rpc.gnosischain.com',
+  1442: 'https://rpc.public.zkevm-test.net',
+  43113: 'https://api.avax-test.network/ext/bc/C/rpc',
+  80001: 'https://polygon-mumbai-bor.publicnode.com',
 }
+
+/**
+ * Supported chains and their `chainId` for the SDK.
+ * @enum
+ */
+export enum SupportedChainId {
+  MAINNET = 1,
+  GOERLI = 5,
+  GNOSIS_CHAIN = 100,
+  POLYZK_TESTNET = 1442,
+  FUJI = 43113,
+  MUMBAI = 80001,
+}
+
 
 export const BASE_PATH = join('..', 'public')
